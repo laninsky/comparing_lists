f4391ecdd35c001dc750b086741d2826bf47eb79
laninsky <laninsky@gmail.com>
2015-05-21 15:12:58 -0500

    Create rename.R

---
+++ rename.R
@@ -0,0 +1,32 @@
+library(stringr)
+
+namelist <- as.matrix(read.table("namelist.txt"))
+
+temp <- readLines("temp")
+
+lentemp <- length(temp)
+
+lennamelist <- dim(namelist)[1]
+
+outtemp <- matrix(NA,nrow=lentemp,ncol=1)
+
+for (i in 1:lentemp) {
+x <- 0
+templine <- unlist(strsplit(temp[i],"\t"))
+lentempline <- length(templine)
+for (j in 1:lentempline) {
+for (k in 1:lennamelist) {
+if ((length(grep(namelist[k,2],templine[j])))>0) {
+x <- 1
+templine[j] <- namelist[k,1]
+break
+}
+}
+if (x==1) {break}
+}
+temp[i] <- paste(templine,collapse="\t")
+}
+
+write.table(temp, "tempout",quote=FALSE, col.names=FALSE,row.names=FALSE)
+
+q()
