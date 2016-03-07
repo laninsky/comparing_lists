a6960588458cff76d413ddeeccec303293b04be6
Alana Alexander <laninsky@gmail.com>
2016-03-07 17:47:05 -0600

    Update SNPs_to_clades.R

--- SNPs_to_clades.R
+++ SNPs_to_clades.R
@@ -36,5 +36,67 @@ temptaxa <- temptaxa[-1,1]
 subsample <- seq(3,(length(temptaxa)*3),3)
 temptaxa <- unlist(strsplit(temptaxa,"\t",fixed=TRUE))[subsample]
 
-temptree
+temptree <- unlist(strsplit(temptree," ",fixed=TRUE))
+temptree <- temptree[length(temptree)]
+temptree <- gsub("\\[.*?\\]","",temptree,fixed=FALSE)
+temptree <- gsub("\\:[0-9]+.[0-9]+[e-]*[0-9]*","",temptree,fixed=FALSE)
+temptree <- gsub(";","",temptree,fixed=TRUE)
+temptree <- unlist(strsplit(temptree,","))
+
+for (i in 1:(length(temptaxa))) {
+for (j in 1:(length(temptree))) {
+temppattern <- gsub("\\(","",temptree[j])
+temppattern <- gsub("\\)","",temppattern)
+if(temppattern==i) {
+temptree[j] <- gsub(i,temptaxa[i],temptree[j])
+break
+}
+}
+}
+
+nodenames <- c(temptaxa,charsetable[1,-1])
+nodenames <- nodenames[duplicated(nodenames)==FALSE]
+nodenames <- nodenames[(length(temptaxa)+1):(length(nodenames))]
+
+nodenames <- sort(as.numeric(nodenames))
+
+recordtaxa <- matrix(NA, ncol=2,nrow=(length(nodenames)))
+recordtaxa[,1] <- nodenames
+
+m <- 0
+
+for (i in 1:(length(temptree))) {
+numopens <- nchar(temptree[i])-nchar(gsub("(","",temptree[i],fixed=TRUE))
+if (numopens > 0 ) {
+for (k in 1:numopens) {
+x <- 1+numopens-k
+m <- m + 1
+j <- i + 1
+while (j <= (length(temptree))) {
+x <- x+nchar(temptree[j])-nchar(gsub("(","",temptree[j],fixed=TRUE))-nchar(temptree[j])+nchar(gsub(")","",temptree[j],fixed=TRUE))
+if (x<=0) {
+break
+}
+j <- j+1
+}
+torecord <- paste(temptree[i:j],collapse=",")
+recordtaxa[m,2] <- torecord
+}
+}
+}
+
+
+recordtaxa[1:dim(recordtaxa)[1],2] <- gsub("(","",recordtaxa[1:dim(recordtaxa)[1],2],fixed=TRUE)
+recordtaxa[1:dim(recordtaxa)[1],2] <- gsub(")","",recordtaxa[1:dim(recordtaxa)[1],2],fixed=TRUE)
+
+recordtaxa <- recordtaxa[order(nchar(recordtaxa[,2])),]
+
+for (i in 1:(dim(recordtaxa)[1])) {
+for (j in (i+1):(dim(recordtaxa)[1])) {
+recordtaxa[j,2] <- gsub(recordtaxa[i,2],recordtaxa[i,1],recordtaxa[j,2],fixed=TRUE)
+}
+}
+
+write.table(recordtaxa,"list_of_clades_in_tree.txt",col.names=FALSE,row.names=FALSE,quote=FALSE)
+
 
